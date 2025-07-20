102f71277bc7f9429cbabe7002ce5ce5dbd14bc1
Andrius0630 <kolendarik123@gmail.com>
2025-07-21 00:15:05 +0300

    Updated configs

--- home_dir/configs/polybar/launch.sh
+++ home_dir/configs/polybar/launch.sh
@@ -4,10 +4,16 @@
 # If all your bars have ipc enabled, you can use 
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
-# killall -q polybar
+#killall -q polybar
+
+#while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+#echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+#polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 
-echo "Bars launched..."
+polybar main &
+if xrandr -q | grep 'HDMI-0 connected'; then
+    polybar secondary &
+fi
+#echo "Bars launched..."
--- home_dir/dotfiles/update_monitor.sh
+++ home_dir/dotfiles/update_monitor.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
-if xrandr -q | grep 'HDMI-0 connected'; then
-	#xrandr --output eDP-1 --auto --output HDMI-1-0 --auto --left-of eDP-1 --primary
+
+if  xrandr -q | grep 'HDMI-0 connected'; then
 	xrandr --setprovideroutputsource modesetting NVIDIA-0
-	xrandr --output eDP-1-1 --auto --output HDMI-0 --auto --left-of eDP-1-1 --primary
-	bspc monitor HDMI-0 -d 1 2 3 4 5
-	bspc monitor eDP-1-1 -d 6 7 8 9 0
+	xrandr --output eDP-1-1 --auto --output HDMI-0 --auto --left-of eDP-1-1 --primary 
+	bspc monitor HDMI-0 -d 1 2 3 4
+	bspc monitor eDP-1-1 -d 5 6 7 8 9 0
 else
-	#xrandr --output eDP-1 --auto
 	xrandr --setprovideroutputsource modesetting NVIDIA-0
 	xrandr --auto
 	bspc monitor -d 1 2 3 4 5 6 7 8 9 0
 fi
+
